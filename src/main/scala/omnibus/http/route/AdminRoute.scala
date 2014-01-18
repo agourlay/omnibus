f61430324b5e4163e194df565703369e370459cb
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2014-01-18 19:08:35 +0100

    close streamed leaves connection after 10s

--- src/main/scala/omnibus/http/route/AdminRoute.scala
+++ src/main/scala/omnibus/http/route/AdminRoute.scala
@@ -51,6 +51,7 @@ class AdminRoute(omnibusService: ActorRef) (implicit context: ActorContext) exte
         path("leaves") {
           get { ctx =>
             omnibusService ! OmnibusServiceProtocol.AllLeaves(ctx.responder)
+            context.system.scheduler.scheduleOnce(10.seconds){ctx.complete("Connection closes")}
           }
         }
       }
