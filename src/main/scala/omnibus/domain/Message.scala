43b9f9e2e70a64e7c7128aa03f4ca2b0c784f944
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2014-02-18 16:18:25 +0100

    setup arbitrary upper bound limit for payload size

--- src/main/scala/omnibus/domain/Message.scala
+++ src/main/scala/omnibus/domain/Message.scala
@@ -8,7 +8,10 @@ import omnibus.domain.topic.TopicPath
 case class Message(id: Long
 	              ,topicPath: TopicPath
 	              ,payload: String
-	              ,timestamp: Long = System.currentTimeMillis / 1000)
+	              ,timestamp: Long = System.currentTimeMillis / 1000){
+	// secure the application by avoiding huge payload (number completely arbitrary)
+	require(payload.size < 100000 , s"Message payload is too big \n")
+}
 
 object MessageObj {
   def toMessageChunk(message: Message): MessageChunk = {
