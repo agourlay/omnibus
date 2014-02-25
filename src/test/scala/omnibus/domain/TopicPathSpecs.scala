7f4373ed78126445e220a145d270cce1d3f7597a
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2014-02-25 11:31:19 +0100

    whats up with scalaTest?

--- src/test/scala/omnibus/domain/TopicPathSpecs.scala
+++ src/test/scala/omnibus/domain/TopicPathSpecs.scala
@@ -2,16 +2,15 @@ package omnibus.test.domain
 
 import omnibus.domain.topic.TopicPath
 import org.scalacheck._
-import Arbitrary._
 import Gen._
 import Prop._
 
 object TopicPathSpecification extends Properties("TopicPath") {
 
-	property("notEmpty") = forAll (listString) { randomList =>
+	val notTooLong = Gen.identifier filter (_.size <= 20)
+    implicit val listString = Gen.nonEmptyContainerOf[List,String](notTooLong)
+
+	property("notEmpty") = forAll(listString) { randomList =>
 	  	!TopicPath(randomList).prettyStr().isEmpty
 	}
-
-	lazy val nonEmptyStr = Gen.alphaStr suchThat ( s => s.size > 0 && s.size <= 20) 
-    lazy val listString = Gen.nonEmptyContainerOf[List,String](nonEmptyStr)
 }  
