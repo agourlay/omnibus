372dadbbe730d564ec3286bbd2181d7999b4cb84
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2014-03-12 14:54:11 +0100

    improve stress test

--- docs/perf/PushSimulation.scala
+++ docs/perf/PushSimulation.scala
@@ -8,23 +8,24 @@ import scala.concurrent.duration._
 import bootstrap._
 import assertions._
 
-class AdvancedExampleSimulation extends Simulation {
-
-	val httpProtocol = http.baseURL("http://localhost:8080")
+class PushSimulation extends Simulation {
 
 	val scenarioOmnibus = scenario("Push like crazy")
 		.repeat(100){
 			exec(
 				http("push on batman")
 					.put("/topics/batman")
-					.body(StringBody("nya"))
+					.body(StringBody("Na na na na na na na na na na na na na na na na... BATMAN!"))
 					.check(status.is(202)))
 		}
 
 	setUp(scenarioOmnibus.inject(ramp(1000 users) over (30 seconds)))
-		.protocols(httpProtocol)
+		.protocols(
+			       http.baseURL("http://localhost:8080")
+			       .warmUp("http://localhost:8080/topics/batman")
+			       )
 		.assertions(
 			global.successfulRequests.percent.is(100)
-		  , details("push on batman").responseTime.max.lessThan(2000)
-		  ,	details("push on batman").requestsPerSec.greaterThan(200))
+		  , global.responseTime.max.lessThan(2000)
+		  ,	global.requestsPerSec.greaterThan(200))
 }
