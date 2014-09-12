a669ba0c4d5ae186a70de0708aecf1e231836ed9
agourlay <arnaud.gourlay@gmail.com>
2014-09-12 12:15:19 +0200

    testing react=replay

---
+++ src/test/scala/omnibus/perf/SimpleReplay.scala
@@ -0,0 +1,43 @@
+package omnibus.test.perf
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+import omnibus._
+
+class SimpleReplay extends Simulation {
+
+	// starting app
+	val app = omnibus.Boot
+	val publishNumber = 1000
+
+	val scenarioOmnibus = scenario("Test replay")
+			.exec(
+				http("create topic")
+				    .post("/topics/batman")
+				    .check(status.is(201)))
+			.exec(
+				http("topic existence")
+				    .get("/topics/batman")
+					.check(status.is(200)))
+			.repeat(publishNumber){
+				exec(
+					http("push on topic")
+						.put("/topics/batman")
+						.body(StringBody("Na na na na na na na na na na na na na na na na... BATMAN!"))
+				)
+			}
+			.exec(ws("Subscribe to topic").open("/streams/topics/batman?react=replay")
+				.check(wsAwait.within(5 seconds).expect(publishNumber)))
+
+		
+
+	setUp(scenarioOmnibus.inject(atOnceUsers(1)))
+		.protocols(
+			       http.baseURL("http://localhost:8080")
+			           .wsBaseURL("ws://localhost:8081")
+			       )
+		.assertions(
+			global.successfulRequests.percent.is(100))
+}
